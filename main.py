86399f709cdf822cf87276eaed0cc6c2789b3c09
Nithya Alfonisa <nithsnisha@gmail.com>
2026-03-17 19:13:41 +0530

    Add files via upload
    code started

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("AI Cyber Deception System Starting")
+import pandas as pd
+import random
+
+data = []
+
+for i in range(50):
+    data.append({
+        "ip": f"192.168.0.{random.randint(1,255)}",
+        "attack_type": random.choice(["DDoS", "Brute Force", "SQL Injection"]),
+        "attempts": random.randint(1, 100),
+        "threat_level": random.choice(["Low", "Medium", "High"])
+    })
+
+df = pd.DataFrame(data)
+df.to_csv("data/honeypot_logs.csv", index=False)
+
+print("Dataset created successfully")
