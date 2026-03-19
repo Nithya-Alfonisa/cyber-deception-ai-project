4e23cb56ae781eab2583e85b4d226727a9235a64
Nithya Alfonisa <nithsnisha@gmail.com>
2026-03-19 12:26:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import pandas as pd
 import random
+import os
+
+os.makedirs("data", exist_ok=True)
 
 data = []
 
-for i in range(50):
-    data.append({
-        "ip": f"192.168.0.{random.randint(1,255)}",
-        "attack_type": random.choice(["DDoS", "Brute Force", "SQL Injection"]),
-        "attempts": random.randint(1, 100),
-        "threat_level": random.choice(["Low", "Medium", "High"])
-    })
+for i in range(50):  # 50 attack sessions
+    attack = random.choice(["DDoS", "Brute Force", "SQL Injection"])
+    base_attempts = random.randint(5, 30)
+
+    for t in range(1, 4):  # time steps (1,2,3)
+        attempts = base_attempts * t  # increasing pattern
+
+        # LOGIC
+        if attempts > 70:
+            threat = "High"
+        elif attempts > 40:
+            threat = "Medium"
+        else:
+            threat = "Low"
+
+        data.append({
+            "ip": f"192.168.0.{random.randint(1,255)}",
+            "attack_type": attack,
+            "attempts": attempts,
+            "time_step": t,
+            "threat_level": threat
+        })
 
 df = pd.DataFrame(data)
 df.to_csv("data/honeypot_logs.csv", index=False)
 
-print("Dataset created successfully")
+print("Dataset with time behavior created")
